f892f1f7eff0fa1d401f9c07729ddddc2b102f2d
Kabir058 <riduanulkabir01@gmail.com>
2025-05-30 23:34:29 +0600

    taask 1

--- tasks1.js
+++ tasks1.js
@@ -1,1 +1,6 @@
 let parag= "Amar desher naam bangladesh. Amar deshe Aam ache, alo ache" 
+let para =(parag.toLowerCase())
+console.log(para)
+let len = para.split('a');
+console.log(len)
+console.log(len.length-1)
