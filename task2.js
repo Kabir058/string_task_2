8ea57bf68ae19d2f2a681d989d420858167aeb70
Kabir058 <riduanulkabir01@gmail.com>
2025-05-31 00:46:50 +0600

    taask 2

---
+++ task2.js
@@ -0,0 +1,24 @@
+// let paragrap= "Hello! My name is Kabir. I Am a student of CSE. Hall, It's place that I loved very much. I hAve some memories , Amiable persons, Adepted ,Adroited, Abide by rules. Assault me there was impossible ,unexpected, ambiguious"
+
+// let para = paragrap.split('a');
+// console.log(para.length-1)
+// let cappara = paragrap.split('A')
+// console.log(cappara.length-1)
+
+let words1 = ['Kabir', 'is', 'a', 'legend'];
+console.log(words1.join(" ")); // "Kabir is a legend"
+
+
+let arr = ['K', 'a', 'b', 'i', 'r'];
+let backToString = arr.join("");
+console.log(backToString); // "Kabir"
+
+
+let name = "Kabir";
+let chars = name.split("");  // split by empty string
+console.log(chars); // ['K', 'a', 'b', 'i', 'r']
+
+
+let sentence = "Kabir is a legend";
+let words = sentence.split(" ");
+console.log(words); // ['Kabir', 'is', 'a', 'legend']
